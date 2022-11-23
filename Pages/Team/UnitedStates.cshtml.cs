ff4c49a5d7eb0ea13ec357059d3157d662255f83
RiggityRussell <russellgtarlt@gmail.com>
2022-11-22 21:21:15 -0500

    Maps

---
+++ Pages/Team/UnitedStates.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CryptidProject.Data;
+using CryptidProject.Models;
+
+namespace CryptidProject.Pages.Team
+{
+    public class UnitedStatesModel : PageModel
+    {
+        private readonly CryptidProject.Data.CryptidProjectContext _context;
+
+        public UnitedStatesModel(CryptidProject.Data.CryptidProjectContext context)
+        {
+            _context = context;
+        }
+
+      public Cryptids Cryptids { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Cryptids == null)
+            {
+                return NotFound();
+            }
+
+            var cryptids = await _context.Cryptids.FirstOrDefaultAsync(m => m.ID == id);
+            if (cryptids == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Cryptids = cryptids;
+            }
+            return Page();
+        }
+    }
+}
